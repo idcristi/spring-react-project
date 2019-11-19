8917defb9dfbb6d239c16bc8171ab9807d2e88c9
idcristi <cristi.duluta@gmail.com>
2019-11-19 20:29:42 +0200

    feat: Add Owner DAO class

---
+++ src/main/java/com/cristi/cardatabase/domain/Owner.java
@@ -0,0 +1,55 @@
+package com.cristi.cardatabase.domain;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+public class Owner {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long ownerid;
+    private String firstname, lastname;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy="owner")
+    private List<Car> cars;
+
+    public List<Car> getCars() {
+        return cars;
+    }
+
+    public void setCars(List<Car> cars) {
+        this.cars = cars;
+    }
+
+    public Owner() {
+    }
+
+    public Owner(String firstname, String lastname) {
+        this.firstname = firstname;
+        this.lastname = lastname;
+    }
+
+    public long getOwnerid() {
+        return ownerid;
+    }
+
+    public void setOwnerid(long ownerid) {
+        this.ownerid = ownerid;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+}
