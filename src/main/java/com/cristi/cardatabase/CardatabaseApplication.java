81d477e63ea3b9b050e60b816ad3e6fbd1da2deb
idcristi <cristi.duluta@gmail.com>
2019-11-20 11:42:32 +0200

    chore: save user in database

--- src/main/java/com/cristi/cardatabase/CardatabaseApplication.java
+++ src/main/java/com/cristi/cardatabase/CardatabaseApplication.java
@@ -1,9 +1,6 @@
 package com.cristi.cardatabase;
 
-import com.cristi.cardatabase.domain.Car;
-import com.cristi.cardatabase.domain.CarRepository;
-import com.cristi.cardatabase.domain.Owner;
-import com.cristi.cardatabase.domain.OwnerRepository;
+import com.cristi.cardatabase.domain.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -13,31 +10,38 @@ import org.springframework.context.annotation.Bean;
 @SpringBootApplication
 public class CardatabaseApplication {
 
-	@Autowired
-	private CarRepository carRepository;
+    @Autowired
+    private CarRepository carRepository;
 
-	@Autowired
-	private OwnerRepository ownerRepository;
+    @Autowired
+    private OwnerRepository ownerRepository;
 
-	public static void main(String[] args) {
-		SpringApplication.run(CardatabaseApplication.class, args);
-	}
+    @Autowired
+    private UserRepository userRepository;
 
-	@Bean
-	CommandLineRunner runner() {
-		return args -> {
-			Owner owner1 = new Owner("John", "Johnson");
-			Owner owner2 = new Owner("Mary", "Robinson");
-			ownerRepository.save(owner1);
-			ownerRepository.save(owner2);
+    public static void main(String[] args) {
+        SpringApplication.run(CardatabaseApplication.class, args);
+    }
 
+    @Bean
+    CommandLineRunner runner() {
+        return args -> {
+            Owner owner1 = new Owner("John", "Johnson");
+            Owner owner2 = new Owner("Mary", "Robinson");
+            ownerRepository.save(owner1);
+            ownerRepository.save(owner2);
 
-			Car car = new Car("Ford", "Mustang", "Red", "ADF-1121", 2017, 59000, owner1);
-			carRepository.save(car);
-			car = new Car("Nissan", "Leaf", "White", "SSJ-3002", 2014, 29000, owner2);
-			carRepository.save(car);
-			car = new Car("Toyota", "Prius", "Silver", "KKO-0212", 2018, 39000, owner2);
-			carRepository.save(car);
-		};
-	}
+            carRepository.save(new Car("Ford", "Mustang", "Red", "ADF-1121",
+                    2017, 59000, owner1));
+            carRepository.save(new Car("Nissan", "Leaf", "White", "SSJ-3002",
+                    2014, 29000, owner2));
+            carRepository.save(new Car("Toyota", "Prius", "Silver", "KKO-0212",
+                    2018, 39000, owner2));
+
+            // username: user password: user
+            userRepository.save(new User("user", "$2a$04$1.YhMIgNX/8TkCKGFUONWO1waedKhQ5KrnB30fl0Q01QKqmzLf.Zi", "USER"));
+            // username: admin password: admin
+            userRepository.save(new User("admin", "$2a$04$KNLUwOWHVQZVpXyMBNc7JOzbLiBjb9Tk9bP7KNcPI12ICuvzXQQKG", "ADMIN"));
+        };
+    }
 }
